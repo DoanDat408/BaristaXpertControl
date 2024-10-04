74755c662a19960f5423e6c687e3f71d0a7993f1
DoanDat408 <doantranthanhdatzz3@gmail.com>
2024-10-04 10:27:38 +0700

    Implemented CRUD operations for User and Role

---
+++ BaristaXpertControl.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BaristaXpertControl.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BaristaXpertControl.Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; } 
+    }
+}
